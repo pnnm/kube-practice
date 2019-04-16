9b71770eb69aea7b9773d9183bd02cf8c9715404
Anil Saladi <anil.saladi-ext@ignitionone.com>
2019-04-16 20:14:03 +0530

    changed docker file repo name

--- src/main/java/com/bank/app/AccountServiceApplication.java
+++ src/main/java/com/bank/app/AccountServiceApplication.java
@@ -3,7 +3,6 @@ package com.bank.app;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-//@EnableDiscoveryClient
 @SpringBootApplication
 public class AccountServiceApplication {
 
